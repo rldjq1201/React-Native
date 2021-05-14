1a3a385d092fecc9005cf2f7afbf9d422a0f60b4
Jgiup <gu.jeong@gymmon.work>
2021-05-15 01:50:28 +0900

    commit test

---
+++ AwesomeTSProject/App.tsx
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+ import React from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+
+ /*const Section: React.FC<{
+   title: string;
+ }> = ({children, title}) => {
+   const isDarkMode = useColorScheme() === 'dark';
+   return (
+     <View style={styles.sectionContainer}>
+       <Text
+         style={[
+           styles.sectionTitle,
+           {
+             color: isDarkMode ? Colors.white : Colors.black,
+           },
+         ]}>
+         {title}
+       </Text>
+     </View>
+   );
+ };*/
+
+ const App = () => {
+   const isDarkMode = useColorScheme() === 'dark';
+
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   };
+
+   return (
+     <View>
+       <Text>Hello World!</Text>
+     </View>
+   );
+ };
+
+ const styles = StyleSheet.create({
+  
+ });
+
+ export default App;
